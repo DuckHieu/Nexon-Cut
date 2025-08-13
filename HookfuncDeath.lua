acb97169f6736cf02f01ca0bcb8c751f5666c763
DuckHieu <114010369+DuckHieu@users.noreply.github.com>
2025-08-13 23:04:43 +0700

    Create HookfuncDeath.lua

---
+++ HookfuncDeath.lua
@@ -0,0 +1,3 @@
+hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function()end)
+hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function()end)
+hookfunction(require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC,function() end)
